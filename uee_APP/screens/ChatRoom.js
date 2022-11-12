cb8aaeaee8dcd9b136c49278a7026576a7a4cd92
ThameeraFernando <sithnuwan111@gmail.com>
2022-11-12 17:58:35 +0530

    add chat rooom to students

--- uee_APP/screens/ChatRoom.js
+++ uee_APP/screens/ChatRoom.js
@@ -1,5 +1,12 @@
 import { useLayoutEffect, useEffect, useState } from "react";
-import { Text, View, StyleSheet, TextInput, FlatList, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import IconButton from "../components/icons/IconButton";
 import MessageText from "../components/MessageText";
 import TeacherNoticeGirdTitle from "../components/TeacherNoticeGridTile";
@@ -7,61 +14,86 @@ import { useAppContext } from "../context/appContext";
 
 const ChatRoom = ({ route, navigation }) => {
   const [message, setMessage] = useState("");
-  const { user, getAllMessages, messages, 
-    sendMessage} = useAppContext()
+  const { user, getAllMessages, messages, sendMessage } = useAppContext();
+
+  const { teacherFname } = route.params;
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Group chat",
     });
   }, [navigation]);
 
-  const handleSendMessage = () => {
-    sendMessage({
-        messageSender:user.firstName,
-        chatRoomOwner:user.firstName, 
-        message:message
-    })
-      setMessage('')
-  }
+  useEffect(
+    (isFocused) => {
+      if (user.type === "teacher") {
+        getAllMessages(user.firstName);
+      } else {
+        getAllMessages(teacherFname);
+      }
+    },
+    [isFocused]
+  );
 
-  const renderNoticeItem = () => {
-    return (
-      messages.map((message)=>{
-        if (message.messageSender==user.firstName) {
-          message.messageSender='You'
-        }
-        return <MessageText key={message._id} message={message.message} sender={message.messageSender} styleProp={message.messageSender=='You'&&{
-          alignSelf:'flex-end'
-        }}/>   
-      })
-           
-    );
+  const handleSendMessage = () => {
+    if (user.type === "teacher") {
+      sendMessage({
+        messageSender: user.firstName,
+        chatRoomOwner: user.firstName,
+        message: message,
+      });
+    } else {
+      sendMessage({
+        messageSender: user.firstName,
+        chatRoomOwner: teacherFname,
+        message: message,
+      });
+      if (user.type === "teacher") {
+        getAllMessages(user.firstName);
+      } else {
+        getAllMessages(teacherFname);
+      }
+    }
+    setMessage("");
   };
 
-  useEffect(() => {
-    getAllMessages(user.firstName)
-  }, [messages]);
+  const renderNoticeItem = () => {
+    return messages.map((message) => {
+      if (message.messageSender == user.firstName) {
+        message.messageSender = "You";
+      }
+      return (
+        <MessageText
+          key={message._id}
+          message={message.message}
+          sender={message.messageSender}
+          styleProp={
+            message.messageSender == "You" && {
+              alignSelf: "flex-end",
+            }
+          }
+        />
+      );
+    });
+  };
 
   return (
     <View style={styles.container}>
-        
-        <View style={styles.sendMessageSection}>
-            <TextInput
-                style={styles.input}
-                onChangeText={setMessage}
-                value={message}
-                placeholder="Send message"
-            />
-            <IconButton
-                icon="send"
-                color="purple"
-                size={36}
-                onPressProp={handleSendMessage}
-            />
+      <View style={styles.sendMessageSection}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setMessage}
+          value={message}
+          placeholder="Send message"
+        />
+        <IconButton
+          icon="send"
+          color="purple"
+          size={36}
+          onPressProp={handleSendMessage}
+        />
       </View>
-      <ScrollView style={styles.listContainer}>
-        {renderNoticeItem()}
-      </ScrollView>
+      <ScrollView style={styles.listContainer}>{renderNoticeItem()}</ScrollView>
     </View>
   );
 };
@@ -71,8 +103,8 @@ export default ChatRoom;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display:"flex",
-    flexDirection:'column-reverse',
+    display: "flex",
+    flexDirection: "column-reverse",
     padding: 24,
     backgroundColor: "#200364",
   },
@@ -92,18 +124,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "#2d0689",
     width: "100%",
-    marginRight: 4
+    marginRight: 4,
   },
 
   sendMessageSection: {
     display: "flex",
     flexDirection: "row",
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     alignItems: "center",
     marginRight: 20,
   },
 
-  listContainer:{
-    marginTop:20,
-  }
+  listContainer: {
+    marginTop: 20,
+  },
 });
